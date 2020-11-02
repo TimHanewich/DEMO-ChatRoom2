2b552b8e3d4c26b67be4eee1fa59050eebd02460
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-02 16:42:42 -0500

    Refresh function is done

--- javascript.js
+++ javascript.js
@@ -1,6 +1,58 @@
 var uuu = new URL(window.location.href);
 var meetingid = uuu.searchParams.get("meetingid");
-var role = uuu.searchParams.get("role").toLocaleLowerCase();
+var role = uuu.searchParams.get("role");
 
-console.log(meetingid);
-console.log(role);
+//Show the error message if either are null
+
+if (meetingid == null || role == null)
+{
+    document.getElementById("ErrorMsg").classList.remove("hidden");
+    document.getElementById("ErrorMsg").innerText = "Parameters meetingid and/or role were null.";
+    document.getElementById("display").remove();
+}
+else //Normal course of action
+{
+    
+}
+
+
+function Refresh()
+{
+    //Create the req
+    var req = new XMLHttpRequest();
+    req.open("get", "https://timhchatroom.azurewebsites.net/api/GetMeetingChat?meetingid=" + meetingid);
+    req.onreadystatechange = function()
+    {
+        if (req.readyState == 4 && req.status == 200)
+        {
+            //Clear the list of chat items
+            document.getElementById("chatpane").innerHTML = "";
+
+            //Parse it
+            var obj = JSON.parse(req.responseText);
+            obj.forEach(AddMsgToChatPane);
+        }
+    }
+    req.send();
+}
+
+function AddMsgToChatPane(msg)
+{
+    var entry = document.createElement("p");
+    entry.innerText = "<" + msg.Person + ">" + " " + msg.Message;
+    
+    if (msg.Person == "plaintiff")
+    {
+        entry.className = "plaintiffmsg chatmsg";
+    }
+    else if(msg.Person == "defendant")
+    {
+        entry.className = "defendantmsg chatmsg";
+    }
+    else if (msg.Person = "moderator")
+    {
+        entry.className = "mediatormsg chatmsg";
+    }
+
+    document.getElementById("chatpane").appendChild(entry);
+}
